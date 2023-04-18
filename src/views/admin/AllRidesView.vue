7e595685c3f0423061fd5b06fe721f0376b0e336
afuwape <afuwapesunday12@gmail.com>
2023-04-18 10:09:34 +0100

    Feat: completed the all rides page

--- src/views/admin/AllRidesView.vue
+++ src/views/admin/AllRidesView.vue
@@ -5,66 +5,72 @@
             <h2 class="mb-4 text-[1.7rem] font-bold">Account Information</h2>
          </div>
 
-         <div class="w-full" v-for="(count, index) in NUMBER_OF_ROWS" :key="index">
-            <div class="columns relative my-4 grid h-[11rem] grid-cols-4 items-center justify-between bg-brand-gray p-[1.2rem] text-xl font-semibold" v-for="(ride, index) in rideHistory" :key="index">
+         <div class="w-full" v-for="(journey, index) in journeys" :key="index">
+            <div class="grid grid-cols-10 gap-x-7 bg-brand-gray my-4 p-7 text-xl font-semibold" v-for="(ride, key) in journey" :key="key">
                <!-- First Column  -->
-               <div class="flex h-full flex-col items-start justify-around capitalize">
-                  <h2 class="capitalize text-sm">
-                     User ID :<span class="ml-4"> {{ ride.userID }}</span>
-                  </h2>
-                  <h2 class="capitalize text-sm">
-                     Role :<span class="ml-4"> {{ ride.role }}</span>
-                  </h2>
-                  <h2 class="capitalize text-sm">
-                     Cost :<span class="ml-4">{{ ride.cost }}</span>
-                  </h2>
+               <div class="col-span-2 flex h-full flex-col gap-y-3 capitalize">
+                  <div class="flex items-center text-sm">
+                     <span class="whitespace-nowrap">User ID :</span>
+                     <span class="ml-4"> {{ ride.user_id }}</span>
+                  </div>
+                  <div class="flex items-center text-sm">
+                     <span class="whitespace-nowrap">Role :</span>
+                     <span class="ml-4"> {{ ride.role }}</span>
+                  </div>
+                  <div class="flex items-center text-sm">
+                     <span class="whitespace-nowrap">Cost :</span>
+                     <span class="ml-4">{{ ride.cost ?? '*****' }}</span>
+                  </div>
                </div>
 
                <!-- Second Coumn -->
-               <div class="flex h-full flex-col items-start justify-around capitalize">
-                  <h2 class="text-sm">
-                     Journey ID : <span class="ml-4"> {{ ride.journeryId }}</span>
-                  </h2>
-                  <h2 class="text-sm">
-                     Status : <span class="ml-4"> {{ ride.status }}</span>
-                  </h2>
-                  <h2 class="text-sm">
-                     Complete : <span class="ml-4"> {{ ride.complete }}</span>
-                  </h2>
+               <div class="col-span-4 flex h-full flex-col gap-y-3 capitalize">
+                  <div class="flex items-center text-sm">
+                     <span class="whitespace-nowrap">Journey ID:</span>
+                     <span class="ml-4 mr-3 truncate uppercase" :title="ride?.journey_id?.toUpperCase()"> {{ ride?.journey_id }}</span>
+                  </div>
+                  <div class="flex items-center text-sm">
+                     <span class="whitespace-nowrap">Status:</span>
+                     <span class="ml-4"> {{ ride.status }}</span>
+                  </div>
+                  <div class="flex items-center text-sm">
+                     <span class="whitespace-nowrap">Complete:</span>
+                     <span class="ml-4"> {{ ride.complete ?? '*****' }}</span>
+                  </div>
                </div>
 
-               <!-- Third  Column -->
-               <div class="flex h-full flex-col items-start justify-around capitalize">
-                  <h2 class="table">
-                     <OriginIcon />
-                     <span class="ml-4 text-sm ">Origin : {{ ride.origin }}</span>
-                  </h2>
-                  <h2 class="ml-2">
-                     <VerticalLine />
-                  </h2>
-                  <h2 class="table">
-                     <DownLocationIcon />
-                     <span class="ml-4 text-sm">Destination : {{ ride.destination }}</span>
-                  </h2>
-               </div>
-               <!-- Last Column  -->
-               <div class="flex h-full flex-col items-start justify-around capitalize">
-                  <h2 class="mb-4 table justify-start">
-                     <CarIcon /><span class="ml-4 text-sm block uppercase">{{
-                        ride.carModel
-                     }}</span>
-                  </h2>
-                  <h2 class="table">
-                     <TimeIcon />
-                     <span class="ml-4 block uppercase text-sm">{{ ride.start_time }}</span>
-                  </h2>
-                  <h2 class="ml-2">
-                     <VerticalLine />
-                  </h2>
-                  <h2 class="table">
-                     <TimeIcon />
-                     <span class="ml-4 block uppercase text-sm">{{ ride.end_time }}</span>
-                  </h2>
+               <div class="col-span-4 flex gap-x-3">
+                  <div class="grow flex flex-col items-start justify-around capitalize">
+                     <div class="flex items-center text-sm gap-x-4">
+                        <OriginIcon />
+                        <span>Origin: {{ ride.origin ?? '*****' }}</span>
+                     </div>
+                     <h2 class="ml-1.5">
+                        <VerticalLine />
+                     </h2>
+                     <div class="flex items-center text-sm gap-x-4">
+                        <DownLocationIcon />
+                        <span>Destination : {{ ride.destination ?? '*****' }}</span>
+                     </div>
+                  </div>
+
+                  <div class="flex flex-col items-start justify-around capitalize">
+                     <h2 class="mb-4 flex w-full items-center justify-start">
+                        <CarIcon />
+                        <span class="ml-4 text-sm block uppercase">{{ ride.carModel ?? '*****' }}</span>
+                     </h2>
+                     <h2 class="flex w-full items-center">
+                        <TimeIcon />
+                        <span class="ml-4 block uppercase text-sm">{{ ride.start_time ?? '*****' }}</span>
+                     </h2>
+                     <h2 class="ml-2">
+                        <VerticalLine />
+                     </h2>
+                     <h2 class="flex w-full items-center">
+                        <TimeIcon />
+                        <span class="ml-4 block uppercase text-sm">{{ ride.end_time ?? '*****' }}</span>
+                     </h2>
+                  </div>
                </div>
             </div>
          </div>
@@ -81,32 +87,17 @@ import DownLocationIcon from "@/components/icons/DownLocationIcon.vue";
 import CarIcon from "@/components/icons/CarIcon.vue";
 import TimeIcon from "@/components/icons/TimeIcon.vue";
 import VerticalLine from "@/components/icons/VerticalLine.vue";
+import { useQuery } from "@tanstack/vue-query";
+import { journeysQuery } from "@/apis/accountApi";
 
 const currentRoute = routeDetails();
-const NUMBER_OF_ROWS = 100;
-const rideHistory = ref([
-   {
-      userID: "8989HHJG78",
-      role: "Driver",
-      cost: "40000",
-      journeryId: "TTYH478",
-      status: "verified",
-      complete: false,
-      origin: "Owerri",
-      destination: "Lokoja",
-      start_time: "4:00 PM",
-      end_time: "4:00PM",
-      carModel: "Volvo",
-   },
-]);
 currentRoute.name = "All Rides"
-</script>
-<style scoped>
-.columns {
-   grid-template-columns: 2fr 2fr 3fr 1fr;
-}
+const journeys = ref([])
 
-.table {
-   @apply flex w-full items-center;
-}
-</style>
+const { isLoading, isError } = useQuery({
+   queryKey: ['journeys'],
+   queryFn: async () => await journeysQuery(),
+   onSuccess: (data) => journeys.value = data.data.data
+})
+</script>
+
