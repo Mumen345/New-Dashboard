932f67c5e3ab2c0a968e5c82b98ab7d71b9971ce
afuwape sunday <afuwapesunday12@gmail.com>
2023-05-03 20:20:09 +0100

    added approve modal for users page

---
+++ src/components/common/ConfirmModal.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+   hidden: Boolean,
+   toggle: {
+      type: Boolean,
+      default: true
+   }
+});
+</script>
+
+<template>
+   <div id="popup-modal" tabindex="-1" :class="hidden ? 'flex' : 'hidden'" class="modal-backdrop overflow-y-auto overflow-x-hidden fixed inset-0 z-[500] h-full items-center">
+      <div class="relative p-4 w-full max-w-md mx-auto h-auto wb-scroll-sm scroll-sm">
+         <div class="relative bg-white rounded-lg shadow">
+            <button v-if="toggle" type="button"
+               class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center" @click="$emit('close')">
+               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg text-base" viewBox="0 0 16 16">
+                  <path
+                     d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+               </svg>
+            </button>
+            <div class="py-7 px-4 sm:px-6 text-center">
+               <slot />
+            </div>
+         </div>
+      </div>
+   </div>
+</template>
+
+<style scoped>
+.modal-backdrop {
+   background: rgba(0, 0, 0, 0.5);
+}
+
+.modal {
+   position: relative;
+   max-height: calc(100% - 3rem);
+   margin-top: 1.5rem;
+   margin-bottom: 1.5rem;
+}
+</style>
--- src/views/admin/UsersView.vue
+++ src/views/admin/UsersView.vue
@@ -1,85 +1,10 @@
-<template>
-   <main-layout>
-      <div class="mb-4">
-         <action-bar :showAction="isCheckedAll" :checkBoxIsClicked="checkedCheckbox" @search="(text) => search = text">
-            <template v-slot:action>
-               <a class="block text-sm w-full hover:bg-neutral-100 px-5 py-2.5 cursor-pointer">
-                  View
-               </a>
-               <a class="block text-sm w-full hover:bg-neutral-100 px-5 py-2.5 cursor-pointer">
-                  Delete
-               </a>
-            </template>
-
-            <template v-slot:filter>
-               <div class="flex flex-col justify-between mb-5">
-                  <list-item checked="asc" v-model="filters.sort_name" @click="doFilter('name')">
-                     Name (A-Z)
-                  </list-item>
-
-                  <list-item checked="asc" v-model="filters.sort_email" @click="doFilter('email')">
-                     Email (A-Z)
-                  </list-item>
-               </div>
-            </template>
-         </action-bar>
-
-         <div class="mt-7 mb-8 overflow-x-auto rounded border bg-white px-0 sm:py-3 sm:px-3">
-            <table class="table" id="table" width="100%">
-               <thead class="thead-light">
-                  <tr class="uppercase">
-                     <th>
-                        <input type="checkbox" @click="selectAll" />
-                     </th>
-                     <th>Name</th>
-                     <th>Email</th>
-                     <th>Phone Number</th>
-                     <th>State</th>
-                     <th>Created At</th>
-                     <th>Actions</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <tr v-for="user in userFinalData" :key="user.id">
-                     <td>
-                        <input type="checkbox" class="checkbox" :checked="isCheckedAll" @click="updateCheckbox(user.id)" />
-                     </td>
-                     <td>{{ user.name }}</td>
-                     <td>{{ user.email }}</td>
-                     <td>{{ user.phone_number }}</td>
-                     <td>
-                        <span :class="useStatusStyle(user.status)" class="flex w-32 items-center justify-center rounded p-2 capitalize text-white">
-                           {{ useStatusContent(user.status) }}</span>
-                     </td>
-                     <td>{{ formatDate(user.created_at) }}</td>
-                     <td>
-                        <div class="static">
-                           <a type="button" class="text-lg font-bold text-neutral-400 hover:text-blue-500" data-dropdown-toggle="dropdown" data-popper-placement="bottom-end">
-                              <i class="material-icons pointer-events-none">more_horiz</i>
-                           </a>
-                           <div class="z-10 hidden w-48 rounded bg-white py-5">
-                              <router-link :to="`/admin/users/${user.id}`" class="block w-full cursor-pointer px-5 py-2 text-base hover:bg-blue-100">View</router-link>
-                              <a class="block w-full cursor-pointer px-5 py-2 text-base hover:bg-blue-100">
-                                 Delete
-                              </a>
-                           </div>
-                        </div>
-                     </td>
-                  </tr>
-               </tbody>
-            </table>
-         </div>
-      </div>
-   </main-layout>
-</template>
-
 <script setup>
 import { ref, reactive, onMounted, computed, watch, onUnmounted } from "vue";
 import { routeDetails } from "@/stores/routeDetails.js";
 import MainLayout from "@/layouts/MainLayout.vue";
-import ActionComponent from "@/components/reusables/ActionComponent.vue";
 import ActionBar from "@/components/common/ActionBar.vue";
 import ListItem from "@/components/ListItem.vue";
+import ConfirmModal from "@/components/common/ConfirmModal.vue";
 
 import { getUsers, getUsersProfile } from "@/apis/accountApi";
 import { useStatusStyle } from "@/composables/useStatusStyle";
@@ -91,18 +16,17 @@ import { useSearchKeyword } from "@/stores/useSearchKeyword";
 const store = useSearchKeyword();
 const search = ref('')
 const { sortAsc, sortDesc } = useRecordSort()
+const approveModal = ref(false)
+const selectedUser = ref({})
+const isCheckedAll = ref(false);
+const checkedCheckbox = ref(false);
+const selectedIds = ref([]); // check ids
 
 let filters = reactive({
    sort_name: '',
    sort_email: '',
 })
 
-
-const isCheckedAll = ref(false);
-const checkedCheckbox = ref(false);
-
-// check ids
-const selectedIds = ref([]);
 
 const isSelected = (id) => {
    return selectedIds.value.includes(id);
@@ -220,3 +144,93 @@ onUnmounted(() => {
 });
 
 </script>
+<template>
+   <main-layout>
+      <div class="mb-4">
+         <action-bar :showAction="isCheckedAll" :checkBoxIsClicked="checkedCheckbox" @search="(text) => search = text">
+            <template v-slot:action>
+               <a class="block text-sm w-full hover:bg-neutral-100 px-5 py-2.5 cursor-pointer">
+                  View
+               </a>
+               <a class="block text-sm w-full hover:bg-neutral-100 px-5 py-2.5 cursor-pointer">
+                  Delete
+               </a>
+            </template>
+
+            <template v-slot:filter>
+               <div class="flex flex-col justify-between mb-5">
+                  <list-item checked="asc" v-model="filters.sort_name" @click="doFilter('name')">
+                     Name (A-Z)
+                  </list-item>
+
+                  <list-item checked="asc" v-model="filters.sort_email" @click="doFilter('email')">
+                     Email (A-Z)
+                  </list-item>
+               </div>
+            </template>
+         </action-bar>
+
+         <div class="mt-7 mb-8 overflow-x-auto rounded border bg-white px-0 sm:py-3 sm:px-3">
+            <table class="table" id="table" width="100%">
+               <thead class="thead-light">
+                  <tr class="uppercase">
+                     <th>
+                        <input type="checkbox" @click="selectAll" />
+                     </th>
+                     <th>Name</th>
+                     <th>Email</th>
+                     <th>Phone Number</th>
+                     <th>State</th>
+                     <th>Created At</th>
+                     <th>Actions</th>
+                  </tr>
+               </thead>
+               <tbody>
+                  <tr v-for="user in userFinalData" :key="user.id">
+                     <td>
+                        <input type="checkbox" class="checkbox" :checked="isCheckedAll" @click="updateCheckbox(user.id)" />
+                     </td>
+                     <td>{{ user.name }}</td>
+                     <td>{{ user.email }}</td>
+                     <td>{{ user.phone_number }}</td>
+                     <td>
+                        <span :class="useStatusStyle(user.status)" class="flex w-32 items-center justify-center rounded p-2 capitalize text-white">
+                           {{ useStatusContent(user.status) }}</span>
+                     </td>
+                     <td>{{ formatDate(user.created_at) }}</td>
+                     <td>
+                        <div class="static">
+                           <a type="button" class="text-lg font-bold text-neutral-400 hover:text-blue-500" data-dropdown-toggle="dropdown" data-popper-placement="bottom-end">
+                              <i class="material-icons pointer-events-none">more_horiz</i>
+                           </a>
+                           <div class="z-10 hidden w-48 rounded bg-white py-5">
+                              <router-link :to="`/admin/users/${user.id}`" class="block w-full cursor-pointer px-5 py-2 text-base hover:bg-blue-100">View</router-link>
+                              <a class="block w-full cursor-pointer px-5 py-2 text-base hover:bg-blue-100" @click="e => { selectedUser = user; approveModal = true }">
+                                 Approve user
+                              </a>
+                              <a class="block w-full cursor-pointer px-5 py-2 text-base hover:bg-blue-100">
+                                 Delete
+                              </a>
+                           </div>
+                        </div>
+                     </td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+      </div>
+
+      <!-- Modal section -->
+      <confirm-modal :hidden="approveModal" @close="approveModal = false">
+         <div class="text-base text-slate-700 text-center mb-7 mt-5">
+            You are about to approve the selected user account
+            please proceed by clicking the button below. if you are sure of this.
+         </div>
+
+         <div class="flex items-center justify-end gap-x-3">
+            <button class="btn-light px-5 py-2.5 rounded-md" @click="e => { approveModal = false; selectedUser = {} }">Cancel</button>
+            <button class="btn-primary px-5 py-2.5 rounded-md">Procced</button>
+         </div>
+      </confirm-modal>
+   </main-layout>
+</template>
